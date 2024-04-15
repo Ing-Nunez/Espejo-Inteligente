5e816db74e7d1d7e7931373150d9e5628fbbf79b
Raul NM <ing.raulnm@gmail.com>
2024-04-15 10:38:21 -0600

    Interfaz con reloj

--- Espejo.py
+++ Espejo.py
@@ -1,16 +1,25 @@
-
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtCore import QTimer, QTime
+from PyQt5.uic import loadUi
+import time
 
-class Ventana(QMainWindow):
-    def __init__(self) -> None:
-        super().__init__()
+class VentanaPrincipal(QMainWindow):
+    def __init__(self):
+        super(VentanaPrincipal, self).__init__()
+        loadUi('Interfaz1.ui', self)
 
+        timer = QTimer(self)
+        timer.timeout.connect(self.displayTime)
+        timer.start(1000)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = Ventana()
-    window.show();
-    sys.exit(app.exec())
-       
+    def displayTime(self):
+        currentTime = QTime.currentTime()
+        displayText = currentTime.toString('hh:mm:ss')
+        self.reloj(displayText)
+        self.lcdnumber.display(displayText)
+
+app = QApplication(sys.argv)
+main = VentanaPrincipal()
+main.show()
+sys.exit(app.exec_())
